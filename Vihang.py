2a1cfcd36ef8294a8bd940bf8b4e073c237904f4
vihangwhite <75125449+vihangwhite@users.noreply.github.com>
2021-08-15 13:06:50 +0530

    Add files via upload

---
+++ Vihang.py
@@ -0,0 +1,19 @@
+introString=input("Enter String:")
+characterCount=0
+wordCount=1
+for i in introString:
+    characterCount=characterCount+1
+    if (i==' '):
+         wordCount = wordCount+1
+print( "No of words in the string:")
+print(wordCount)
+print("Number of Characters in the string")
+print(characterCount)
+
+ while chances < 5
+ 
+if guess == number:
+    print("Congratulations YOU WON!!!")
+    break
+if not chances < 5:
+    print("YOU LOSE!!! The number is", number)
